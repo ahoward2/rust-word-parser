af3d7573e97ac8f25e0060355874c91ce79fb660
Austin Howard <ahoward2@unca.edu>
2022-02-21 15:01:51 -0500

    do the count on all files in the directory

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Rust Word Parser
 
-A rust program that reads a text file and counts the number of words.
+A rust program that reads all text files from a directory and tells you their
+total word counts.
+
+Take it easy on me, this is my first rust program :).
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::fs;
+use std::path::Path;
 
 fn main() {
 
-	let contents = fs::read_to_string("../words.txt").expect("Something went wrong reading the file");
+	let path = Path::new("../text-files");
 
-	println!("\nFull content of the text file: \n\n{}",contents);
+	for entry in fs::read_dir(path).expect("Unable to read directory") {
 
-	let split = contents.split(" ");
+		let entry = entry.expect("Unable to get entry");
+		
+		let entry_path = entry.path();
 
-	let vec = split.collect::<Vec<&str>>();
-	
-	println!("Word count: {}", vec.len());
+		let entry_name = entry.file_name();
 
+		let unwrapped_name = entry_name.to_str().unwrap();
+
+		let contents = fs::read_to_string(entry_path).expect("Something went wrong reading the file");
+
+		println!("\nFull content of the file {name}: \n\n{file_contents}", name=unwrapped_name, file_contents=contents);
+
+		let new_content = &contents.replace("\n", "");
+
+		let split = new_content.split(" ");
+
+		let vec = split.collect::<Vec<&str>>();
+
+		println!("Word count: {}", vec.len());
+	}
 }
